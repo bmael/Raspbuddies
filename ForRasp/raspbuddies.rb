3cf978cac201426da211918bfa23b4457e7b9561
bmael <maelbar44@gmail.com>
2013-03-28 11:23:46 +0100

    Try to broadcast a message with the M2 broadcast but does not work

--- ForRasp/raspbuddies.rb
+++ ForRasp/raspbuddies.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 require 'bud'
 require 'backports'	
-require_relative 'raspbuddies_protocol'
+require_relative './raspbuddies_protocol'
+require_relative './broadcast/CausalBroadcast'
+require 'socket'
 
 class Raspbuddies
   include Bud
@@ -10,11 +12,27 @@ class Raspbuddies
   def initialize(id, server, opts={})
     @id = id
     @server = server
+
     super opts
   end
   
   bootstrap do
+	ipC, portC = ip_port.split(":")
+	@ca = CausalAgent.new("T#{@id}" , 
+	                      :ext_ip => ipC,
+	                      :ext_port => portC.to_i)
+	@ca.run_bg
+	@ca.initProcess
+	
+	ipS, portS = @server.split(":")
+	
     connect <~ [[@server, ip_port, @id]]
+	
+	@ca.sync_do {
+	  @ca.pipe_join <+ [[@server]]
+	}
+	puts "Sleepinf for 5 seconds to wait others clients..."
+	sleep(5)
   end
   
   ############################################################################
@@ -23,21 +41,22 @@ class Raspbuddies
   
   # Receive a message
   bloom :rcv do 
-	stdio <~ mcast { |m| [["LOG received message", logIntoFile(m.val)]] if m.val[0]!=ip_port} # don't log if we are the msg sender
-	stdio <~ mcast { |m| [["Receiving my message", m.val]] if m.val[0]==ip_port} # advise that you received your message
+	stdio <~ mcast { |m| [["LOG received message #{logIntoFile(m.val)}" ]] if m.val[0]!=ip_port} # don't log if we are the msg sender
+	stdio <~ mcast { |m| [["Receiving my message #{m.val}"]] if m.val[0]==ip_port} # advise that you received your message
 
   end
   
   # New clients detected by central server
   bloom :update_nodelist do
-	stdio <~ new_client { |c| [["new client", c.val]]}
-	stdio <~ nodelist{ |c| [["nodelist", c.val]]}
+# 	stdio <~ new_client { |c| [["new client", c.val]]}
+# 	stdio <~ nodelist{ |c| [["nodelist", c.val]]}
   end
    
   ###########################################################################
   ##							RUBY METHODS								 ##
   ###########################################################################
    # Log a message into the file log.
+  #TODO buffer for msg before log it
   def logIntoFile(msg)
     File.open("log/log" << ip_port, 'a') {|f| f.write(msg) 
 				f.write("\n")  }
@@ -46,12 +65,26 @@ class Raspbuddies
     
   #method to send a message
   def sendMsg() 
-	  mcast <~ [[@server, [ip_port, "", "Hello from " << ip_port, "" ]]]
-	  stdio <~ [["Sending a message [puts]"]]
+	      var = Bud::MapLattice.new({0=>Bud::MaxLattice.new(0),1=>Bud::MaxLattice.new(0),2=>Bud::MaxLattice.new(1),3=>Bud::MaxLattice.new(0),
+				4=>Bud::MaxLattice.new(0),5=>Bud::MaxLattice.new(1),6=>Bud::MaxLattice.new(0),7=>Bud::MaxLattice.new(0),
+				8=>Bud::MaxLattice.new(0),9=>Bud::MaxLattice.new(4),10=>Bud::MaxLattice.new(0),11=>Bud::MaxLattice.new(0),
+				12=>Bud::MaxLattice.new(0),13=>Bud::MaxLattice.new(5),14=>Bud::MaxLattice.new(0),15=>Bud::MaxLattice.new(1),
+				16=>Bud::MaxLattice.new(0)})
+		  @ca.sync_do{
+			
+	  @ca.bcast_send <+ [[[@server, [ip_port, "", "Hello from " << ip_port, "" ]], [3, var]]]
+			}
+# 	  @ca.inclock <+ [[0,var]] # Throws an exception like undifined method ??
+# 	  mcast <~ [[@server, [ip_port, "", "Hello from " << ip_port, "" ]]]
+	  stdio <~ [["Sending a message..."]]
   end
   
   def sendMsg2(wait_time)
 	return [@server, [ip_port, "", "Hello from " << ip_port, "" ]]
+  end
+  
+  def stopProcess
+	@ca.stop
   end
     
 end
@@ -59,12 +92,19 @@ end
 
 server = (ARGV.length == 2) ? ARGV[1] : RaspbuddiesProtocol::DEFAULT_ADDR
 	
-puts "Server address: #{server}"
+
+puts "------------------------------------------"
+puts "                Run Client"
+puts "  Server address: #{server}"
+# # puts "  Private ip : #{Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address}"
+puts "------------------------------------------"
+
 program = Raspbuddies.new(ARGV[0], server, :stdin => $stdin )
 
 program.run_bg
 
   nbMessage = 10 
+  nbMessageReceived = 0
   
   sleep 2
   
@@ -76,7 +116,12 @@ program.run_bg
   end
   
   sleep 10
-  
+   
+program.stopProcess
+   
+puts "------------------------------------------"
+puts "                   End"
+puts "------------------------------------------"
 program.stop
 
 
